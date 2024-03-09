718f6a84837091adc3fc5febd4a4f411acbefdfe
lifaiho0916 <phoenixdev1223@gmail.com>
2024-03-09 15:55:23 +0800

    commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
